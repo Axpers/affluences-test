6cb808606c2a4b03691ba571649448d28dcb69b5
GarikD <garikd@outlook.fr>
2022-05-06 18:02:39 +0200

    Refactor : Query model validation

--- src/reservations/models/reservation-query.model.ts
+++ src/reservations/models/reservation-query.model.ts
@@ -3,17 +3,14 @@ import { IsDateString, IsDefined, IsNotEmpty, IsString } from 'class-validator';
 export class ReservationQuery {
   @IsDefined()
   @IsNotEmpty()
-  @IsString()
   resourceId: string;
 
   @IsDefined()
   @IsNotEmpty()
-  @IsString()
   @IsDateString()
   date: string;
 
   @IsDefined()
   @IsNotEmpty()
-  @IsString()
   hour: string;
 }
